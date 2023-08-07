104a4c10a2aac392467b8fc81425c7be74a55848
joaquin-soriano <88865050+joaquin-soriano@users.noreply.github.com>
2023-08-07 15:25:34 -0300

    Create Local Admins.ps1
    bla

---
+++ Local Admins.ps1
@@ -0,0 +1,48 @@
+$admins = Get-LocalGroupMember -Group "Administradores"
+$GG_ADM_WKS = "GMIRGOR\GG-Admin WKS"
+$ADM_LOCAL = $env:COMPUTERNAME + "\Administrador"
+$DOM_ADM = "GMIRGOR\Domain Admins"
+$sendermailaddress = "Seguridad_Informatica@mirgor.com.ar"            
+$SMTPserver = "192.168.5.91"
+$asuntoBASE = "Administradores locales de maquina "
+$body = "Los siguientes usuarios son adminstradores locales de la maquina: " + "`r`n`r`n"
+[int]$nroAdmins = 0
+
+function SendMail ($SMTPserver,$sendermailaddress,$mailBody,$asuntoFinal)            
+{            
+    $smtpServer = $SMTPserver            
+    $msg = new-object Net.Mail.MailMessage            
+    $smtp = new-object Net.Mail.SmtpClient($smtpServer)            
+    $msg.From = $sendermailaddress            
+    $msg.To.Add("joaquin.soriano@mirgor.com.ar")   
+    $msg.Subject = $asuntoFinal           
+    $msg.Body = $mailBody            
+    $smtp.Send($msg)            
+}
+
+
+foreach($administrador in $admins)
+{
+    if($administrador.Name -ne $GG_ADM_WKS -and $administrador.Name -ne $ADM_LOCAL -and $administrador.Name -ne $DOM_ADM)
+    {
+        $nroAdmins += 1
+        $body += $administrador.Name + "`r`n`r`n"
+    }
+}
+
+$body += "****FIN****"
+
+$asuntoOK = $asuntoBASE + $env:COMPUTERNAME
+
+try
+{
+
+    if($nroAdmins -gt 0)
+    {
+        SendMail $SMTPserver $sendermailaddress $body $asuntoOK
+        
+    }
+} catch
+{
+    Write-Output $body
+}
